448f021cb0377d4a684484f3c779b2090dd69c3b
EmirSab <emir.sabanic.ro@gmail.com>
2020-05-30 17:57:45 +0200

    addDtoForDriverAndRepository

---
+++ Drive/Dtos/DriverForCreationDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Drive.Dtos
+{
+    public class DriverForCreationDto
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Jmbg { get; set; }
+        public string BankAccount { get; set; }
+        public DateTime WorkStart { get; set; }
+        public string Adress { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Drive/Dtos/DriverForDetailDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Drive.Dtos
+{
+    public class DriverForDetailDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Jmbg { get; set; }
+        public string BankAccount { get; set; }
+        public DateTime WorkStart { get; set; }
+        public DateTime? WorkEnd { get; set; }
+        public string Adress { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
