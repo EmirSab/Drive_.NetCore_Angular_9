c5cd6c13ccceb6631d2df55f97eadee301613f78
EmirSab <emir.sabanic.ro@gmail.com>
2020-06-01 22:24:33 +0200

    AddedAutoMapper.cs

---
+++ Drive/Helpers/AutoMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Drive.Dtos;
+using Drive.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Drive.Helpers
+{
+    public class AutoMapper : Profile
+    {
+        public AutoMapper()
+        {
+            CreateMap<DriverForCreationDto, Driver>();
+            CreateMap<Driver, DriverToReturnDto>();
+
+        }
+    }
+}
